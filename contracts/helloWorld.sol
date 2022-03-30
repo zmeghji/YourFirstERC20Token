262e5210c867f70a5005e094b34877d9a6edf5c5
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-03-29 20:34:56 -0400

    initial commit

---
+++ contracts/helloWorld.sol
@@ -0,0 +1,14 @@
+
+//This is the solidity language version
+pragma solidity ^0.8.0;
+
+contract HelloWorld  //contract is similar to a class in C#/Java
+{
+    string public greet = "Hello World!"; //declared a public field with the value Hello World
+
+    /* Exercise
+            Declare a public field called meaningOfLife which returns 42
+            Hint: the type for integer is uint
+    */
+    
+}
